93282fa5107152c40f9bbff0ada806661de0edaf
Sam Chong <samchong@Sams-Mac-mini.local>
2022-12-21 14:47:04 +0000

    Adds code to check if the guessed letter is in the word.

--- milestone_3.py
+++ milestone_3.py
@@ -6,4 +6,10 @@ while True:
        break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
- 
+
+word = ""
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
