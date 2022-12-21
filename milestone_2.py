84f8c5e1cde87e4415a2cb3582c708e39e09f8f5
Sam Chong <samchong@Sams-Mac-mini.local>
2022-12-21 11:33:05 +0000

    Adds code to check user's guess is valid.

--- milestone_2.py
+++ milestone_2.py
@@ -9,3 +9,10 @@ word = random.choice(word_list)
 print(word)
 
 guess = input("Please enter a single letter: ")
+
+
+if (len(guess) == 1) and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+
