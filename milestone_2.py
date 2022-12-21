9247a2550cfad377ee36917e773997da1b0431be
Sam Chong <samchong@Sams-Mac-mini.local>
2022-12-21 12:54:10 +0000

    Reinstates comnments in code.

--- milestone_2.py
+++ milestone_2.py
@@ -1,15 +1,18 @@
 
 import random
 
+# Creates initial list of words
 word_list = ["melon", "pear", "cherry", "kiwi", "watermelon"]
 print(word_list)
 
+# Selects a word randomly from the list
 word = random.choice(word_list)
 print(word)
 
+# Asks the user for a guess
 guess = str(input("Please enter a single letter: "))
 
-
+# Checks if the guess is a single letter
 if (len(guess) == 1) and guess.isalpha():
     print("Good guess!")
 else:
