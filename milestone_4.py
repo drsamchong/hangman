7733927ac958877dbc5fd7e4c8da67e6c784519c
Sam Chong <samchong@Sams-Mac-mini.local>
2023-01-05 19:57:08 +0000

    Modifies parameter list for initialisation to specify type of num_lives.

--- milestone_4.py
+++ milestone_4.py
@@ -1,3 +1,22 @@
+# %%
+import random
+
 class Hangman():
-    def __init__(self):
-        pass
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_' for char in [*self.word]]
+        self.num_letters = len(self.word)
+        self.list_of_guesses= []
+
+# %%
+words = ['horse', 'cat', 'elephant']
+
+game = Hangman(words)
+# %%
+print(game.word)
+print(game.word_guessed)
+print(game.num_letters)
+print(game.list_of_guesses)
+# %%
