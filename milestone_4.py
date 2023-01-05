d48df76064af48fe7c9857eba0055db4b8aeed56
Sam Chong <samchong@Sams-Mac-mini.local>
2023-01-05 20:17:14 +0000

    Adds methods to ask user to input a guess and to validate and check if the character is in the word.

--- milestone_4.py
+++ milestone_4.py
@@ -2,13 +2,36 @@
 import random
 
 class Hangman():
-    def __init__(self, word_list, num_lives: int=5):
+    def __init__(self, word_list: list, num_lives: int=5):
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(word_list)
         self.word_guessed = ['_' for char in [*self.word]]
         self.num_letters = len(set(self.word))
-        self.list_of_guesses= []
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again.")
+   
+    def ask_for_input(self):
+        while True:
+            guess = input("Please guess a single letter: ")
+            if not ((len(guess) == 1) and guess.isalpha()):
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
+
+
+ 
+
 
 # %%
 words = ['horse', 'cat', 'elephant']
@@ -19,4 +42,6 @@ print(game.word)
 print(game.word_guessed)
 print(game.num_letters)
 print(game.list_of_guesses)
+# %%
+game.ask_for_input()
 # %%
