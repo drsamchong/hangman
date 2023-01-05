75ee17e3ae6b85f752d353e5dbecfdd2131d593c
Sam Chong <samchong@Sams-Mac-mini.local>
2023-01-05 12:07:19 +0000

    Moves code for guess input and checking against word to function

--- milestone_3.py
+++ milestone_3.py
@@ -1,15 +1,24 @@
 # TODO ask user for input and verify - loop if it doesn't pass
 
-while True:
-    guess = input("Please guess a single letter: ")
-    if (len(guess) == 1) and guess.isalpha():
-       break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
 
 word = ""
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+
+def ask_for_input():
+    while True:
+        guess = input("Please guess a single letter: ")
+        if (len(guess) == 1) and guess.isalpha():
+           break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+ask_for_input()
