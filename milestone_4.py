39bcd3df63c95a2ece4aae769f19385733cca0db
Sam Chong <samchong@Sams-Mac-mini.local>
2023-01-05 20:00:28 +0000

    Modifies num_letters attribute to number of unique characters.

--- milestone_4.py
+++ milestone_4.py
@@ -2,12 +2,12 @@
 import random
 
 class Hangman():
-    def __init__(self, word_list, num_lives=5):
+    def __init__(self, word_list, num_lives: int=5):
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(word_list)
         self.word_guessed = ['_' for char in [*self.word]]
-        self.num_letters = len(self.word)
+        self.num_letters = len(set(self.word))
         self.list_of_guesses= []
 
 # %%
